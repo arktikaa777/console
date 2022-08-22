bf2a7bc101261d80cbb6ff21ebf9bf17edecd117
Genya <arktikaa777@gmail.com>
2022-08-22 23:50:30 +1200

    add Example_04 "Div"

---
+++ Example005_conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hey, this is Masha!");
+}
+else
+{
+    Console.Write("Hi, ");
+    Console.WriteLine(username);
+}
